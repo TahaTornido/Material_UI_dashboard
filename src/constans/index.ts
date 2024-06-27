494ef9a4efbecfd5b5c6d81023d5ad461c35c9c9
ibra-kdbra <git-repo-develop@outlook.com>
2024-06-27 12:50:19 +0300

    export logos

---
+++ src/constans/index.ts
@@ -0,0 +1,51 @@
+import { IoIosSettings } from "react-icons/io";
+import { IoDocumentSharp } from "react-icons/io5";
+import { MdGroups } from "react-icons/md";
+import { FaFolder } from "react-icons/fa";
+import { BsJournalMedical } from "react-icons/bs";
+import { CiCircleList } from "react-icons/ci";
+import { HiMiniFlag } from "react-icons/hi2";
+
+export const nav = [
+	{
+		icon: IoIosSettings,
+	},
+	{
+		icon: IoDocumentSharp,
+	},
+	{
+		icon: MdGroups,
+	},
+	{
+		icon: FaFolder,
+	},
+	{
+		icon: BsJournalMedical,
+	},
+	{
+		icon: CiCircleList,
+	},
+	{
+		icon: HiMiniFlag,
+	},
+];
+export const inputs = [
+	{
+		name: "FirstName",
+	},
+	{
+		name: "LastName",
+	},
+	{
+		name: "Pesel",
+	},
+	{
+		name: "Street",
+	},
+	{
+		name: "City",
+	},
+	{
+		name: "ZipCode",
+	},
+];
